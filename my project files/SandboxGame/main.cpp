ee3cfd50d41a8bf1c3262d7ec2104d3195f2120d
amdreallyfast <amdreallyfast@hotmail.com>
2014-07-21 10:27:30 -0700

    first commit; cleaned up remnants of my_GL_window from main

--- my project files/SandboxGame/main.cpp
+++ my project files/SandboxGame/main.cpp
@@ -11,27 +11,14 @@ int main(int argc, char **argv)
    // must create the application before the custom QGLWidget
    QApplication app(argc, argv);
 
-   //// make the custom QGLWidget
-   //my_GL_window window;
-   //if (!window.initialize()){ return -1; }
-   //window.show();
    My_Game my_game;
-   if (!my_game.initialize())
-   {
-      return -1;
-   }
-
+   if (!my_game.initialize()) { return -1; }
    my_game.go();
 
    // run the application
    int app_err_code = app.exec();
 
-   //// shut down custom QGLWidget code (put in a non-zero value if it fails)
-   //if (!window.shutdown()){ app_err_code |= 1; }
-   if (!my_game.shutdown())
-   {
-      app_err_code |= 1;
-   }
+   if (!my_game.shutdown()) { app_err_code |= 1; }
 
    return app_err_code;
 }
