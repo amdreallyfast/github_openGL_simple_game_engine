96ec1fd0c3adf8f70eb059372e3e214197daf26e
amdreallyfast <amdreallyfast@hotmail.com>
2014-07-21 17:02:53 -0700

    added a Renderer_Component; it's purpose is encapsulate object rendering within the entity itself

---
+++ my project files/Engine/Entities/Components/Renderer_Component.h
@@ -0,0 +1,14 @@
+#ifndef ENGINE_RENDERER_COMPONENT_H
+#define ENGINE_RENDERER_COMPONENT_H
+
+#include "Entities\Game_Component.h"
+
+namespace Entities
+{
+   class Renderer_Component : public Game_Component
+   {
+   public:
+   };
+}
+
+#endif
